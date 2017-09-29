f19a93441e154188605a4f5c6e57490d0585d025
Jeff Zohrab <jzohrab@gmail.com>
2017-09-29 18:52:16 -0400

    Add powershell to start the slave jar.

---
+++ scripts/start_slave.ps1
@@ -0,0 +1,24 @@
+# Runs the swarm-client jar.
+# Sample call:
+# & .\start_slave.ps1 -master_ip 10.1.0.98 -label sensei_build
+param(
+  [Parameter(Mandatory=$true)][string]$master_ip,
+  [Parameter(Mandatory=$true)][string]$label
+)
+
+# "swarmslave" text is important:
+# - role-based strategy in Jenkins master adds this item to the "slavenode" group
+# - API control
+$name = "swarmslave_${env:computername}"
+
+# anonymous access for swarmslave
+$swarm_client_cmd = "java -jar C:/swarm-client.jar " `
+  + "-disableSslVerification " `
+  + "-master 'http://${master_ip}:8080' " `
+  + "-executors 1 -fsroot 'C:\Jenkins' " `
+  + "-name ${name} " `
+  + "-labels $label " `
+  + "-disableClientsUniqueId"
+
+echo $swarm_client_cmd
+iex $swarm_client_cmd
