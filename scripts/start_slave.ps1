15c36267342b3d4d41f8c36aa884e6ee09c9dc1b
Jeff Zohrab <jzohrab@gmail.com>
2017-10-05 13:43:07 -0400

    Comments and clarifying var name.

--- scripts/start_slave.ps1
+++ scripts/start_slave.ps1
@@ -1,8 +1,17 @@
-# Runs the swarm-client jar.
+# Runs the swarm-client jar, connecting to the master's private IP.
+#
 # Sample call:
-# & .\start_slave.ps1 -master_ip 10.1.0.98 -label sensei_build
+# & .\start_slave.ps1 -master_private_ip 10.1.0.98 -label sensei_build
+#
+# According to the docs, Jenkins slaves should be able to auto-discover
+# the master (ref https://wiki.jenkins.io/display/JENKINS/Auto-discovering+Jenkins+on+the+network),
+# but this currenty fails in our AWS setup despite permissive
+# internal security group settings:
+#   WARNING: Failed to receive a reply to broadcast.
+#   java.net.SocketTimeoutException: Receive timed out
+#
 param(
-  [Parameter(Mandatory=$true)][string]$master_ip,
+  [Parameter(Mandatory=$true)][string]$master_private_ip,
   [Parameter(Mandatory=$true)][string]$label
 )
 
@@ -14,7 +23,7 @@ $name = "swarmslave_${env:computername}"
 # anonymous access for swarmslave
 $swarm_client_cmd = "java -jar C:/swarm-client.jar " `
   + "-disableSslVerification " `
-  + "-master 'http://${master_ip}:8080' " `
+  + "-master 'http://${master_private_ip}:8080' " `
   + "-executors 1 -fsroot 'C:\Jenkins' " `
   + "-name ${name} " `
   + "-labels $label " `
